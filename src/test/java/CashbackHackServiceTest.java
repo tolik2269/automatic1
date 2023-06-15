183425087fa1ac7457981586c675f81961b5aaf3
Anatoly Balbutsky <bal.anatolii1985@mail.ru>
2023-06-16 00:31:32 +0300

    v5

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,15 +1,15 @@
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
     @Test
     public void calcCashbackTest(){
         CashbackHackService service=new CashbackHackService();
-        int amaunt=900;
+        int amaunt=1000;
         int actual = service.remain(amaunt);
-        int expected= 100;
-        assertEquals(actual,expected);
+        int expected= 0;
+        assertEquals(expected,actual);
     }
 }
